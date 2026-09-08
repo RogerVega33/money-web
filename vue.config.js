2f1d551e9f6d3970e046c169b991e2554a4a89f2
Roger Vega <vega.roger@hotmail.com>
2026-09-07 19:17:48 -0500

    Improved: logging

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,11 @@ module.exports = defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
+        // Vue actúa como proxy al ejecutar npm run serve.
+        // No aceptar una IP reenviada inventada por el cliente.
+        onProxyReq(proxyReq, req) {
+          proxyReq.setHeader('X-Forwarded-For', req.socket.remoteAddress)
+        },
       },
     },
   },
