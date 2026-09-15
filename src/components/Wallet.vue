483fb4f4532dbefa7f9fa5369d7ff23a2c2aaf37
Roger Vega <vega.roger@hotmail.com>
2026-09-15 12:43:47 -0500

    Improved: El ícono de una billetera cripto archivada se pone de color gris

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -23,8 +23,8 @@
                  :class="{ 'text-blue-700': selectedWallet?.id === wallet.id }"
                  class="group flex items-center space-x-4 text-gray-900 hover:text-blue-500 cursor-pointer">
               <div class="flex-shrink-0">
-                <fa icon="sack-dollar" class="h-8" :class="wallet.isArchived ? 'text-gray-500' : 'text-green-500'" v-if="wallet.type === 'fiat'"/>
-                <fa :icon="['fab', 'bitcoin']" class="text-yellow-300 h-8" v-if="wallet.type === 'crypto'"/>
+                <fa icon="sack-dollar" class="h-8" :class="wallet.isArchived ? 'text-gray-400' : 'text-green-500'" v-if="wallet.type === 'fiat'"/>
+                <fa :icon="['fab', 'bitcoin']" class="h-8" :class="wallet.isArchived ? 'text-gray-400' : 'text-yellow-300'" v-if="wallet.type === 'crypto'"/>
               </div>
               <div class="flex-1 min-w-0">
                 <p class="text-sm truncate group-hover:text-blue-500"
